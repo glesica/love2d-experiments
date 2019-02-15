c66efeacf859439eb1fbf6b89ee3f0b2546ae1a8
George Lesica <george@lesica.com>
2019-02-14 19:10:55 -0700

    Clean up and rename

--- main.lua
+++ main.lua
@@ -1,87 +1,26 @@
-function makeBoid()
-  boid = {
-    x = math.random(0, love.graphics.getWidth()),
-    y = math.random(0, love.graphics.getHeight()),
+function makeAutomaton(rows, cols, prob)
+  local updateDelta = 0.25
 
-    ax = math.random(-10, 11),
-    ay = math.random(-10, 11),
-  }
-
-  boid.x0 = boid.x
-  boid.y0 = boid.y
-
-  boid.xp = boid.x
-  boid.yp = boid.y
-
-  local dx = math.random(-100, 101)
-  local dy = math.random(-100, 101)
-
-  boid.x = boid.xp + dx * (1 / 60) + 0.5 * boid.ax * (1 / 60) ^ 2
-  boid.y = boid.yp + dy * (1 / 60) + 0.5 * boid.ay * (1 / 60) ^ 2
-
-  return boid
-end
-
-function drawBoid(boid)
-  local dx = (boid.x - boid.xp) * 10
-  local dy = (boid.y - boid.yp) * 10
-
-  love.graphics.setColor(1.0, 1.0, 1.0, 0.5)
-  love.graphics.line(boid.x, boid.y, boid.x + boid.ax, boid.y + boid.ay)
-  love.graphics.line(boid.x, boid.y, boid.x + dx, boid.y + dy)
-
-  love.graphics.setColor(0.7, 0.2, 0.1)
-  love.graphics.circle('fill', boid.x, boid.y, 7)
-
-  love.graphics.setColor(0.0, 1.0, 0.2)
-  love.graphics.circle('fill', boid.x + boid.ax, boid.y + boid.ay, 3)
-
-  love.graphics.setColor(0.0, 0.2, 1.0)
-  love.graphics.circle('fill', boid.x + dx, boid.y + dy, 3)
-end
-
-function distance(x0, y0, x1, y1)
-  return math.sqrt((x1 - x0)^2 + (y1 - y0)^2)
-end
-
-function updateBoid(boid, dt)
-  local xp = boid.x
-  local yp = boid.y
-
-  boid.x = boid.x + (boid.x - boid.xp) * (dt / dtp) + boid.ax * dt ^ 2
-  boid.y = boid.y + (boid.y - boid.yp) * (dt / dtp) + boid.ay * dt ^ 2
-
-  boid.xp = xp
-  boid.yp = yp
-
-  -- Calculate the deltas from our start so we can update acceleration
-  dxh = boid.x0 - boid.x
-  dyh = boid.y0 - boid.y
-
-  boid.ax = dxh * 0.1
-  boid.ay = dyh * 0.1
-end
-
-function makeAutomaton(rows, cols, prob, perTick)
   local auto = {
     rows = rows,
     cols = cols,
-    cells = {},
-    buffer = {},
-    tick = perTick,
-    perTick = perTick
+    cellBuffer = {},
+    cellStates = {},
+    nextUpdate = updateDelta,
+    simTime = 0,
+    updateDelta = updateDelta
   }
 
   for r = 0, rows do
-    auto.cells[r] = {}
-    auto.buffer[r] = {}
+    auto.cellStates[r] = {}
+    auto.cellBuffer[r] = {}
     for c = 0, cols do
       local value = 0
       if math.random() < prob then
         value = 1
       end
-      auto.cells[r][c] = value
-      auto.buffer[r][c] = 0
+      auto.cellStates[r][c] = value
+      auto.cellBuffer[r][c] = 0
     end
   end
 
@@ -89,11 +28,11 @@ function makeAutomaton(rows, cols, prob, perTick)
 end
 
 function makeGlider(auto, row, col)
-  auto.cells[row + 0][col + 2] = 1
-  auto.cells[row + 1][col + 2] = 1
-  auto.cells[row + 2][col + 2] = 1
-  auto.cells[row + 2][col + 1] = 1
-  auto.cells[row + 1][col + 0] = 1
+  auto.cellStates[row + 0][col + 2] = 1
+  auto.cellStates[row + 1][col + 2] = 1
+  auto.cellStates[row + 2][col + 2] = 1
+  auto.cellStates[row + 2][col + 1] = 1
+  auto.cellStates[row + 1][col + 0] = 1
 end
 
 function updateAutomaton(auto)
@@ -108,34 +47,34 @@ function updateAutomaton(auto)
       local ce = (c + 1) % mc -- Col East
 
       local total =
-        auto.cells[rn][c] +  -- N
-        auto.cells[rn][ce] + -- NE
-        auto.cells[r][ce] +  -- E
-        auto.cells[rs][ce] + -- SE
-        auto.cells[rs][c] +  -- S
-        auto.cells[rs][cw] + -- SW
-        auto.cells[r][cw] +  -- W
-        auto.cells[rn][cw]   -- NW
+        auto.cellStates[rn][c] +  -- N
+        auto.cellStates[rn][ce] + -- NE
+        auto.cellStates[r][ce] +  -- E
+        auto.cellStates[rs][ce] + -- SE
+        auto.cellStates[rs][c] +  -- S
+        auto.cellStates[rs][cw] + -- SW
+        auto.cellStates[r][cw] +  -- W
+        auto.cellStates[rn][cw]   -- NW
       
-      auto.buffer[r][c] = auto.cells[r][c]
+      auto.cellBuffer[r][c] = auto.cellStates[r][c]
 
       if total < 2 then
-        auto.buffer[r][c] = 0
+        auto.cellBuffer[r][c] = 0
       end
 
       if total == 3 then
-        auto.buffer[r][c] = 1
+        auto.cellBuffer[r][c] = 1
       end
 
       if total > 3 then
-        auto.buffer[r][c] = 0
+        auto.cellBuffer[r][c] = 0
       end
     end
   end
 
-  local temp = auto.cells
-  auto.cells = auto.buffer
-  auto.buffer = temp
+  local temp = auto.cellStates
+  auto.cellStates = auto.cellBuffer
+  auto.cellBuffer = temp
 end
 
 function drawAutomaton(auto)
@@ -145,12 +84,22 @@ function drawAutomaton(auto)
   local cellWidth = width / auto.cols
   local cellHeight = height / auto.rows
 
-  love.graphics.setColor(0.7, 0.7, 0.1)
   for r = 0, auto.rows do
     for c = 0, auto.cols do
-      local value = auto.cells[r][c]
+      local value = auto.cellStates[r][c]
       if value == 1 then
-        love.graphics.rectangle('fill', c * cellWidth + 1, r * cellHeight + 1, cellWidth - 2, cellHeight - 2)
+        love.graphics.setColor(
+          0.7,
+          0.7,
+          0.1
+        )
+        love.graphics.rectangle(
+          'fill',
+          c * cellWidth + 1,
+          r * cellHeight + 1,
+          cellWidth - 2,
+          cellHeight - 2
+        )
       end
     end
   end
@@ -166,13 +115,13 @@ function toggleCell(auto, x, y)
   local r = math.floor(y / cellHeight)
   local c = math.floor(x / cellWidth)
 
-  auto.cells[r][c] = math.abs(auto.cells[r][c] - 1)
+  auto.cellStates[r][c] = math.abs(auto.cellStates[r][c] - 1)
 end
 
 -- Love2D callbacks
 
 function love.load()
-  auto = makeAutomaton(30, 30, 0.0, 0.25)
+  auto = makeAutomaton(30, 30, 0.0)
   makeGlider(auto, 5, 5)
   makeGlider(auto, 5, 15)
   makeGlider(auto, 5, 25)
@@ -183,7 +132,6 @@ function love.load()
   makeGlider(auto, 25, 15)
   makeGlider(auto, 25, 25)
 
-  tick = 0
   isPaused = true
 end
 
@@ -191,11 +139,11 @@ function love.update(dt)
   if isPaused then
     return
   end
-  if tick > auto.tick then
-    auto.tick = auto.tick + auto.perTick
+  if auto.simTime > auto.nextUpdate then
+    auto.nextUpdate = auto.nextUpdate + auto.updateDelta
     updateAutomaton(auto)
   end
-  tick = tick + dt
+  auto.simTime = auto.simTime + dt
 end
 
 function love.draw()
